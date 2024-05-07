95e5686eb038a1fa4bfba4a4aeb86aa9f5a6a4af
RickDvn <168721035+RickDvn@users.noreply.github.com>
2024-05-07 13:22:12 +0200

    JavaDoc incorporado

---
+++ doc/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.viewnext.batchdb"},{"l":"com.viewnext.batchdb.config"},{"l":"com.viewnext.batchdb.listener"},{"l":"com.viewnext.batchdb.model"},{"l":"com.viewnext.batchdb.processor"},{"l":"com.viewnext.batchdb.reader"},{"l":"com.viewnext.batchdb.writer"}];updateSearchResults();
